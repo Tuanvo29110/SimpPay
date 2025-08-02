160a739f7962f338a463103c8b2b165c6e3404f3
typical <53472591+TypicalShavonne@users.noreply.github.com>
2025-08-02 19:40:29 +0700

    add HandlerRegistry logging

--- simppay-paper/src/main/java/org/simpmc/simppay/handler/HandlerRegistry.java
+++ simppay-paper/src/main/java/org/simpmc/simppay/handler/HandlerRegistry.java
@@ -5,6 +5,7 @@ import org.simpmc.simppay.config.ConfigManager;
 import org.simpmc.simppay.config.types.BankingConfig;
 import org.simpmc.simppay.config.types.CardConfig;
 import org.simpmc.simppay.config.types.CoinsConfig;
+import org.simpmc.simppay.util.MessageUtil;
 
 import java.lang.reflect.InvocationTargetException;
 
@@ -35,6 +36,10 @@ public class HandlerRegistry {
         cardHandler = (PaymentHandler) cardConfig.cardApi.handlerClass.getDeclaredConstructor().newInstance();
         bankHandler = (PaymentHandler) bankingConfig.bankApi.handlerClass.getDeclaredConstructor().newInstance();
         coinsHandler = (CoinsHandler) coinsConfig.pointsProvider.handlerClass.getDeclaredConstructor().newInstance();
+        MessageUtil.info("Registered handlers: ");
+        MessageUtil.info("Card Handler: " + cardHandler.getClass().getSimpleName());
+        MessageUtil.info("Bank Handler: " + bankHandler.getClass().getSimpleName());
+        MessageUtil.info("Coins Handler: " + coinsHandler.getClass().getSimpleName());
     }
 
     public void reload() {
